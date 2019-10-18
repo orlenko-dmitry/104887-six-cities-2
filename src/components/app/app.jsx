73baa344c50113ff7d1d408ed2a78134d056d412
Dmitry Orlenko <orlenko.dmitry.dev@gmail.com>
2019-10-18 13:38:42 +0300

    render places in MainPage

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 import MainPage from '../main-page/main-page.jsx';
 
-const App = () => <MainPage />;
+const places = [
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+  `Wood and stone place`,
+];
+
+const App = () => <MainPage places={places}/>;
 
 export default App;
