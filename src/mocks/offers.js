a4263f9bea916e2dfbff848c97dabaf72fd21b97
Dmitry Orlenko <orlenko.dmitry.dev@gmail.com>
2019-10-29 15:09:41 +0200

    add moks

---
+++ src/mocks/offers.js
@@ -0,0 +1,39 @@
+const offers = [{
+  id: 1,
+  name: `Beautiful luxurious apartment at great location`,
+  type: `Apartment`,
+  price: 120,
+  isPremiun: true,
+  inBookmarks: false,
+  rating: `93%`,
+  img: `img/apartment-01.jpg`,
+}, {
+  id: 2,
+  name: `Wood and stone place`,
+  type: `Private room`,
+  price: 80,
+  isPremiun: false,
+  inBookmarks: true,
+  rating: `80%`,
+  img: `img/room.jpg`,
+}, {
+  id: 3,
+  name: `Beautiful luxurious apartment at great location`,
+  type: `Apartment`,
+  price: 70,
+  isPremiun: true,
+  inBookmarks: false,
+  rating: `80%`,
+  img: `img/apartment-02.jpg`,
+}, {
+  id: 4,
+  name: `Beautiful luxurious apartment at great location`,
+  type: `Apartment`,
+  price: 200,
+  isPremiun: false,
+  inBookmarks: false,
+  rating: `80%`,
+  img: `img/apartment-03.jpg`,
+}];
+
+export default offers;
