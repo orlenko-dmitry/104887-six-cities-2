9d1150ffa950a764d8e96995422d7c824144a9e1
Dmitry Orlenko <orlenko.dmitry.dev@gmail.com>
2019-10-29 19:39:09 +0200

    add snapshot test for OffersList and OfferCard

--- src/components/main-page/main-page.test.js
+++ src/components/main-page/main-page.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import MainPage from './main-page.jsx';
+import offers from '../../mocks/offers.js';
 
 it(`MainPage renders correctly`, () => {
-  const tree = renderer.create(<MainPage/>).toJSON();
+  const tree = renderer.create(<MainPage offers={offers}/>).toJSON();
+
   expect(tree).toMatchSnapshot();
 });
