1c2124f97b3a771c1e2d75aa6942b576810fdc19
Dmitry Orlenko <orlenko.dmitry.dev@gmail.com>
2019-11-09 16:45:44 +0200

    add snapshot test to OffersMap

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -4,7 +4,11 @@ import renderer from 'react-test-renderer';
 import App from './app.jsx';
 import offers from '../../mocks/offers.js';
 
-it(`App renders correactly`, () => {
+const div = document.createElement(`div`);
+div.id = `map`;
+document.body.appendChild(div);
+
+it(`App renders correctly`, () => {
   const tree = renderer.create(<App offers={offers}/>).toJSON();
 
   expect(tree).toMatchSnapshot();
--- src/components/offers-map/offers-map.test.js
+++ src/components/offers-map/offers-map.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import MainPage from './main-page.jsx';
+import OffersMap from '../offers-map/offers-map.jsx';
 import offers from '../../mocks/offers.js';
 
 const div = document.createElement(`div`);
 div.id = `map`;
 document.body.appendChild(div);
 
-it(`MainPage renders correctly`, () => {
-  const tree = renderer.create(<MainPage offers={offers}/>).toJSON();
+it(`OffersMap renders correctly`, () => {
+  const tree = renderer.create(
+      <OffersMap offers={offers} />
+  ).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
