f6bd533a3b12d2aebdcc2a5a32f8f7eaffe00655
Dmitry Orlenko <orlenko.dmitry.dev@gmail.com>
2019-12-07 11:44:02 +0200

    add react-notifications

--- src/store/data/actions.js
+++ src/store/data/actions.js
@@ -5,7 +5,9 @@ import {
   GET_USER_SUCCESS,
   FETCH_OFFERS_SUCCESS,
   FETCH_COMMENTS_SUCCESS,
+  POST_COMMENTS_PENDING,
   POST_COMMENTS_SUCCES,
+  POST_COMMENTS_ERROR,
 } from '../../consts/actionTypes.js';
 
 import enpoints from '../../consts/endpoints.js';
@@ -57,10 +59,17 @@ export default ({
             }));
   },
   postComment: ({offerId, rating, comment}) => (dispatch, getState, api) => {
+    dispatch({type: POST_COMMENTS_PENDING});
     return api.post(enpoints.comments(offerId), {rating, comment})
             .then((response) => dispatch({
               type: POST_COMMENTS_SUCCES,
-              payload: response.data,
-            }));
+              payload: convertCommentsToCamelCase(response.data),
+            }))
+            .catch((err) => {
+              return {
+                type: POST_COMMENTS_ERROR,
+                payload: err.message,
+              };
+            });
   },
 });
--- src/store/data/data.js
+++ src/store/data/data.js
@@ -5,6 +5,9 @@ import {
   SELECT_CITY,
   FETCH_OFFERS_SUCCESS,
   FETCH_COMMENTS_SUCCESS,
+  POST_COMMENTS_PENDING,
+  POST_COMMENTS_SUCCES,
+  POST_COMMENTS_ERROR,
 } from '../../consts/actionTypes.js';
 import {
   APP_CITIES,
@@ -18,6 +21,7 @@ export const initialState = {
   isAuthorizationRequired: false,
   user: null,
   status: ASYNC_STATUSES.PENDING,
+  postMessageStatus: ``,
 };
 
 export default (state = initialState, {type, payload}) => {
@@ -34,6 +38,12 @@ export default (state = initialState, {type, payload}) => {
       return Object.assign({}, state, {offers: payload, status: ASYNC_STATUSES.SUCCESS});
     case FETCH_COMMENTS_SUCCESS:
       return Object.assign({}, state, {comments: payload});
+    case POST_COMMENTS_PENDING:
+      return Object.assign({}, state, {postMessageStatus: ASYNC_STATUSES.PENDING});
+    case POST_COMMENTS_SUCCES:
+      return Object.assign({}, state, {comments: payload, postMessageStatus: ASYNC_STATUSES.SUCCESS});
+    case POST_COMMENTS_ERROR:
+      return Object.assign({}, state, {postMessageStatus: ASYNC_STATUSES.ERROR});
     default: return state;
   }
 };
